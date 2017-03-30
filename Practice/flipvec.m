8607b5f15984ed6d926e8949eb7e4e8a7a6a1330
Cleophas Kalekem <kalekemcleophas01@gmail.com>
2017-03-30 19:16:47 -0400

    Moved practice programs to practice folder and deleted zip files

--- Practice/flipvec.m
+++ Practice/flipvec.m
@@ -1,14 +1,18 @@
 function flip = flipvec(vec)
 
-[r, c] = size(vec);
-
-if r ==1 && c>1
+[r c] = size(vec);
+if (r ==1 && c > 1)
     flip = fliplr(vec);
-   % fprintf('%d is a column vector!\n', flip);
-elseif r>1 && c==1
+elseif (r > 1 && c ==1 )
     flip = flipud(vec);
-   % fprintf('%d is a row vector!\n', flip);
 else
-    disp('Scalar');
+    flip = vec;
 end
 end
+
+
+        
+    
+    
+    
+    
